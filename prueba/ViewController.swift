1d24d1cc10386444559927abcd4a4d12233cec80
Arminor <riosminor.andres@comunidad.unam.mx>
2019-09-17 16:26:58 -0500

    Initial Commit

---
+++ prueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prueba
+//
+//  Created by Laboratorio UNAM-Apple 04 on 9/17/19.
+//  Copyright © 2019 bakchudy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
